710752af8a7fe0468e12301a63957d6650a5455f
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-06-15 20:10:38 -0300

    Add files via upload

---
+++ PHP/index2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GerenciamentoBibllioteca</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    
+    <div class="corpoGerenciar">
+    <h1>BIBLIOTECA</h1>
+    
+    <ul>
+    <li><a href="livros-listar.php"><img class="sair"src="img/pilha-de-livros.PNG"></a></li>
+    <li><a href="autores-listar.php"><img class="sair" src="img/escrevendo (1).PNG"></a></li>
+    <a href="usuario-logout.php"><img class="sair" src="img/sair.PNG"></a>
+
+
+</ul>
+
+
+    
+
+   
+</div>
+
+
+
+</body>
+</html>
+
